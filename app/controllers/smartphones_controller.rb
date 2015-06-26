a4d38542c7a4f2167a04b22583674edb8936228e
finisher1017@gmail.com <finisher1017@gmail.com>
2015-06-26 16:59:54 -0700

    stuck on smartphone edit

--- app/controllers/smartphones_controller.rb
+++ app/controllers/smartphones_controller.rb
@@ -1,26 +1,47 @@
 class SmartphonesController < ApplicationController
 
 def index
-	@smartPhone = Equipment.all
+	@smartPhone = Equipment.where(equipmentType: "Smartphone")
 end
 
 def show
-  @smartPhone = Equipment.where(equipmentType: "Smartphone", equipmentModel: "Galaxy4")
+  @smartPhone = Equipment.where(equipmentType: "Smartphone").find(params[:id])
 end
 
 def new
+end
+
+def edit
+  @smartPhone = Equipment.where(equipmentType: "Smartphone").find(params[:id])
 end
 
 def create
   @smartPhone = Equipment.new(smartphone_params)
  
   @smartPhone.save
-  redirect_to @smartPhone
+  redirect_to smartphone_path(@smartPhone)
+end
+
+def update
+  @smartPhone = Equipment.where(equipmentType: "Smartphone").find(params[:id])
+ 
+  if @smartPhone.update(smartphone_params)
+    redirect_to smartphones_path(@smartPhone)
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @smartPhone = Equipment.where(equipmentType: "Smartphone").find(params[:id])
+  @smartPhone.destroy
+ 
+  redirect_to smartphones_path
 end
 
 private
   def smartphone_params
-    params.require(:smartphone).permit(:equipmentType, :manufacturer,
+    params.require(:smartphone).permit(:id, :equipmentType, :manufacturer,
   									:equipmentModel, :carrier, 
   									:phoneNumber, :serialNumber,
   									:meid, :macAddress, :department,
