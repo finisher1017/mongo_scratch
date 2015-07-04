f17fc079bc8ad8fa0884c68116df0c54b0d64fbb
finisher1017@gmail.com <finisher1017@gmail.com>
2015-07-04 08:38:13 -0700

    Full CRUD

--- app/controllers/wastecontainers_controller.rb
+++ app/controllers/wastecontainers_controller.rb
@@ -1,4 +1,47 @@
 class WastecontainersController < ApplicationController
-  def index
+def index
+  @wastecontainer = Equipment.where(equipmentType: "Waste Container")
+end
+
+def show
+  @wastecontainer = Equipment.where(equipmentType: "Waste Container").find(params[:id])
+end
+
+def new
+end
+
+def edit
+  @wastecontainer = Equipment.where(equipmentType: "Waste Container").find(params[:id])
+end
+
+def create
+  @wastecontainer = Equipment.new(container_params)
+ 
+  @wastecontainer.save
+  redirect_to wastecontainer_path(@wastecontainer)
+end
+
+def update
+  @wastecontainer = Equipment.where(equipmentType: "Waste Container").find(params[:id])
+ 
+  if @wastecontainer.update(container_params)
+    redirect_to wastecontainer_path(@wastecontainer)
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @wastecontainer = Equipment.where(equipmentType: "Waste Container").find(params[:id])
+  @wastecontainer.destroy
+ 
+  redirect_to wastecontainers_path
+end
+
+private
+  def container_params
+    params.require(:wastecontainer).permit(:id, :equipmentType, :manufacturer,
+  									:equipmentModel, :wasteId,
+  									:quantity, :notes )
   end
 end
